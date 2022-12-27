15875c6e2bdd49facdfe6ebe8cff48b544750a63
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-12-28 01:59:25 +0200

    Added SlideShow and css style for this

--- GameShop/ClientApp/src/components/Home/Home.js
+++ GameShop/ClientApp/src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React, {useEffect,useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-import SlideShow from './SlideShow';
+import SlideShowHome from './SlideShowHome';
 
 
 
@@ -45,34 +45,9 @@ export default function Home() {
         return (
 
 
-             <SlideShow />
-            //<div>
-            //    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-            //        <div class="carousel-inner">
-            //            <div class="carousel-item active">
-            //                <img src={dbdata[0].Cover} class="d-block w-100" alt="..." />
-            //            </div>
-            //            <div class="carousel-item">
-            //                <img src="..." class="d-block w-100" alt="..." />
-            //            </div>
-            //            <div class="carousel-item">
-            //                <img src="..." class="d-block w-100" alt="..." />
-            //            </div>
-            //        </div>
-            //        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            //            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            //            <span class="visually-hidden">Previous</span>
-            //        </button>
-            //        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            //            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            //            <span class="visually-hidden">Next</span>
-            //        </button>
-            //    </div>
-
-            //</div>
+             <SlideShowHome />
+            
 
         )
-    //}
-    //else
-    //    return (<> </>)
+    
 }
