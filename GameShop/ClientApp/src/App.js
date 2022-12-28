b84c7fdeec18c1ca3c20f295c8d7a1305587d7d5
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2022-12-28 02:04:51 +0200

    Added new path

--- GameShop/ClientApp/src/App.js
+++ GameShop/ClientApp/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import  Layout  from './components/NavMenu/Layout';
-import  Home    from './components/Home/Home';
-
+import Home from './components/Home/Home';
+import PurchasePage from './components/PurchasePage/PurchasePage';
 
 import './custom.css'
 
@@ -11,8 +11,9 @@ export default function App() {
     return (
         <Layout>
 
-        <Route exact path='/' component={Home} />
-        
+            <Route exact path='/' component={Home} />
+            <Route exact path='/Purchase' component={PurchasePage} />
+
       </Layout>
     );
   
