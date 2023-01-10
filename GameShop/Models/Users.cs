00eb47f9867052fb0efa8f22cc3b420ccff87c3b
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-01-10 17:31:45 +0200

    Created new class Users

---
+++ GameShop/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.Models
+{
+    public class Users
+    {
+
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Username { get; set; }
+
+
+        public string Password { get; set; }
+
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+    }
+}
