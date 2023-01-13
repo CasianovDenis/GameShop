2c3594141cd2ff7e0d32863189a6a4f034426490
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-01-14 01:04:30 +0200

    Remade NavMenu class in to NavMenu function + hooks

--- GameShop/ClientApp/src/components/NavMenu/NavMenu.js
+++ GameShop/ClientApp/src/components/NavMenu/NavMenu.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import user_icon from '../public_files/user_icon.png';
 import logo from './Logo.png';
@@ -10,156 +10,150 @@ import './NavMenu.css';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import GetCookie from '../public_files/GetCookie.js';
+
 import Mongo_upload_file from './Mongo_upload_file';
 import Modal_add_game from './Modal_add_game';
 
-export class NavMenu extends Component {
-    static displayName = NavMenu.name;
+
+export default function NavMenu(props) {
+
+    const [collapsed, setCollapsed] = useState(true);
+    const [admin_rights, setAdminRights] = useState(null);
+    const [request, setRequest] = useState(true);
+    const [username, setUserName] = useState("");
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
 
 
-    constructor(props) {
-        
-        super(props);
 
-        
+    useEffect(() => {
 
-        this.toggleNavbar = this.toggleNavbar.bind(this);
-
-        
-        let user_name = GetCookie("username");
-        let requestdata="";
+        setUserName(GetCookie("username"));
 
         if (GetCookie("status_account") == "online") {
 
+            if (request == true) {
 
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    "Username":user_name
-                })
-            };
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        "Username": username
+                    })
+                };
 
 
-           
-            //call api from backend and send json data,which create before
-            
+
+                //call api from backend and send json data,which create before
+
                 fetch('http://localhost:56116/api/get_user_role', requestOptions)
                     .then(response => response.json())
                     .then((responseData) => {
 
-                        requestdata = responseData;
-                      
-                        console.log(requestdata);
+                        if (responseData == "admin")
+                            setAdminRights("block");
+                        else
+                            setAdminRights("none");
+
+                        setRequest(false);
                     });
-
-            
-
-
-            if (requestdata == "admin") {
-
-                this.state = {
-                    collapsed: true,
-                    status_link: "none",
-                    status_dropdown: "block",
-                    username: user_name,
-                    role_dropdown: "block"
-                };
             }
-
-            else {
-               
-                this.state = {
-                    collapsed: true,
-                    status_link: "none",
-                    status_dropdown: "block",
-                    username: user_name,
-                    role_dropdown: "none"
-                };
-            }
-
-            
-        }
-        else {
-            this.state = {
-                collapsed: true,
-                status_link: "block",
-                status_dropdown: "none",
-                role_dropdown: "none"
-            };
         }
 
 
 
-    }
+    }, [admin_rights]);
 
-    toggleNavbar() {
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
+   const ExitFromAccount = () => {
 
-    Exit = () => {
-        var now = new Date();
+       var now = new Date();
         var time = now.getTime();
         time += 3600 * 1000;
         now.setTime(time);
 
-        window.localStorage.clear();
+
 
         document.cookie = "username= ; expires =" + now.toUTCString();
 
         document.cookie = "status_account=; expires = " + now.toUTCString();
 
-        this.history.push('/');
+       const redirect = useHistory();
+
+       redirect.go('/');
     }
 
-    render() {
+    if (admin_rights != null) {
+
         return (
-            <header>
+
+            <div>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow mb-3" light style={{ backgroundColor: "#2a2a2a" }}>
                     <Container>
-                        <NavbarBrand tag={Link} to="/" style={{ color: "white" }}><img src={logo} style={{ width: "60px", height: "50px" }} />GameShop</NavbarBrand>                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                        <NavbarBrand tag={Link} to="/" style={{ color: "white" }}><img src={logo} style={{ width: "60px", height: "50px" }} />GameShop</NavbarBrand>
+                        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+
+                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={collapsed} navbar>
                             <ul className="navbar-nav flex-grow">
-
-                                <NavItem>
-                                    <NavLink tag={Link} to="/SignIn"
-                                        style={{ display: this.state.status_link, color: "white" }}>Log In</NavLink>
-                                </NavItem>
-
-                                <NavItem>
-                                    <NavLink tag={Link} to="/SignUp"
-                                        style={{ display: this.state.status_link, color: "white" }}>Sign Up</NavLink>
-                                </NavItem>
-
-                                <NavItem style={{ display: this.state.status_dropdown }}>
+                              
+                                <NavItem >
                                     <div class="dropdown">
 
                                         <img src={user_icon} class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown"
-                                            aria-haspopup="true" aria-expanded="false" style={{width:"50px",height:"50px",cursor:"pointer"} } />
+                                            aria-haspopup="true" aria-expanded="false" style={{ width: "50px", height: "50px", cursor: "pointer" }} />
 
                                         <div class="dropdown-menu " aria-labelledby="dropdownMenuButton" >
 
-                                            <p class="dropdown-item" style={{ cursor: "pointer" }}>Name : {this.state.username}</p>
+                                            <p class="dropdown-item" style={{ cursor: "pointer" }}>Name : {username}</p>
                                             <NavLink tag={Link} class="dropdown-item" to="/Account" >Account</NavLink>
                                             <NavLink tag={Link} class="dropdown-item" to="/Settings" >Settings</NavLink>
 
-                                            <div style={{ display: this.state.role_dropdown }}>
-                                            <Modal_add_game  />
-                                                <Mongo_upload_file  />
+                                            <div style={{ display:  admin_rights  }}>
+
+                                                <Modal_add_game />
+                                                <Mongo_upload_file />
+
                                             </div>
 
-                                            <NavLink tag={Link} class="dropdown-item" onClick={this.Exit} >Exit</NavLink>
+                                            <NavLink tag={Link} class="dropdown-item" onClick={ExitFromAccount} >Exit</NavLink>
                                         </div>
                                     </div>
 
                                 </NavItem>
                             </ul>
                         </Collapse>
+
+
                     </Container>
                 </Navbar>
-            </header>
+            </div>
         );
-    }
+    
+       
+}
+    else
+    return (
+        <div>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow mb-3" light style={{ backgroundColor: "#2a2a2a" }}>
+                    <Container>
+                    <NavbarBrand tag={Link} to="/" style={{ color: "white" }}><img src={logo} style={{ width: "60px", height: "50px" }} />GameShop</NavbarBrand>
+                    <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={collapsed} navbar>
+                            <ul className="navbar-nav flex-grow">
+
+                                <NavItem>
+                                    <NavLink tag={Link} to="/SignIn"
+                                        style={{  color: "white" }}>Log In</NavLink>
+                                </NavItem>
+
+                               <NavItem>
+                                    <NavLink tag={Link} to="/SignUp"
+                                        style={{  color: "white" }}>Sign Up</NavLink>
+                                </NavItem>
+
+                           </ul>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+        </div>
+    );
 }
