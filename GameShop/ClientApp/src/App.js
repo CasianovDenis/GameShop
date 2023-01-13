22e0a8f9762bea245262e992611c05f8b36432f9
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-01-13 19:49:26 +0200

    Was added path for new page

--- GameShop/ClientApp/src/App.js
+++ GameShop/ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import  Layout  from './components/NavMenu/Layout';
 import Home from './components/Home/Home';
 import PurchasePage from './components/PurchasePage/PurchasePage';
+import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 
 import './custom.css'
@@ -14,6 +15,7 @@ export default function App() {
 
             <Route exact path='/' component={Home} />
             <Route exact path='/Purchase' component={PurchasePage} />
+            <Route exact path='/SignIn' component={SignIn} />
             <Route exact path='/SignUp' component={SignUp} />
 
       </Layout>
