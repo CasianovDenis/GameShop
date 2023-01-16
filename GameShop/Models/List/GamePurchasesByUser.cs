8dc0d3bc9efa2e7c4acf836b2b102f02bc8a8eb4
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-01-16 16:26:55 +0200

    Created class for list

---
+++ GameShop/Models/List/GamePurchasesByUser.cs
@@ -0,0 +1,20 @@
+﻿namespace GameShop.Models.List
+{
+    public class GamePurchasesByUser
+    {
+        public string GameName { get; set; }
+
+        public string KeyOfGame { get; set; }
+
+        public string URL_Image { get; set; }
+
+
+        public GamePurchasesByUser(string gamename, string key, string url)
+        {
+
+            GameName = gamename;
+            KeyOfGame = key;
+            URL_Image = url;
+        }
+    }
+}
