d076c868aa0db30f0e9ac653681d54d01ace6beb
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-01-31 19:44:57 +0200

    Writed new api for get commentary using range

--- GameShop/Controllers/GamesCommentaryController.cs
+++ GameShop/Controllers/GamesCommentaryController.cs
@@ -1,5 +1,6 @@
 ﻿using GameShop.Models;
 using GameShop.Models.DBConnection;
+using GameShop.Models.TempClasses;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
@@ -20,13 +21,14 @@ namespace GameShop.Controllers
 
         [Route("~/api/get_game_commentary")]
         [HttpPost]
-        public JsonResult GetGameCommentary(GamesCommentary gamescomment)
+        public JsonResult GetGameCommentary(TempGamesCommentary tempgamescomment)
         {
 
             try
             {
 
-                var commentary = _conString.GamesCommentary.Where(data => data.GameName == gamescomment.GameName).OrderByDescending(data => data.ID).ToList();
+                var commentary = _conString.GamesCommentary.Where(data => data.GameName == tempgamescomment.GameName)
+                         .OrderByDescending(data => data.ID).Take(tempgamescomment.number_displaying_comment).ToList();
 
                 return Json(commentary);
             }
@@ -37,6 +39,27 @@ namespace GameShop.Controllers
                 return Json("No commentary for this game");
             }
         }
+
+        [Route("~/api/get_number_of_commentary")]
+        [HttpPost]
+        public JsonResult GetNumberOfCommentary(TempGamesCommentary tempgamescomment)
+        {
+
+            try
+            {
+
+                var commentary = _conString.GamesCommentary.Where(data => data.GameName == tempgamescomment.GameName).ToList();
+
+                return Json(commentary.Count);
+            }
+            catch (Exception exception)
+            {
+
+
+                return Json(exception);
+            }
+        }
+
 
         [Route("~/api/add_game_commentary")]
         [HttpPost]
