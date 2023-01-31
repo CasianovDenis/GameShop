599a3e4a9def5cd4e8d5a5c22b1424a45294313c
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-01-31 19:40:47 +0200

    Added button for scroll to top of the page

---
+++ GameShop/ClientApp/src/components/public_files/ScrollTopButton.js
@@ -0,0 +1,42 @@
+﻿import React, { useState } from 'react';
+import style from './ScrollTopButton.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function ScrollTopButton() {
+
+    const [show_top_button, setShowTopButton] = useState(false);
+
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 500) {
+            setShowTopButton(true);
+        }
+        else
+            setShowTopButton(false);
+    });
+
+
+
+    return (
+        
+        <>
+
+            {show_top_button && (
+
+                <div className={style.top_button} >
+                    <FontAwesomeIcon icon={faArrowUp} style={{ height: "25px", marginTop: "12px", marginLeft: "15px" }} onClick={goToTop} />
+                </div>
+            )}
+
+            </>
+
+    )
+}
