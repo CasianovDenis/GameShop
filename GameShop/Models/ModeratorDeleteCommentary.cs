6c4762e09ffc3c3a0b8afe38239db7a77fc22145
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-02-06 13:12:29 +0200

    Moderator classes where exist 2 fields moderator name and rights token

---
+++ GameShop/Models/ModeratorDeleteCommentary.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.Models
+{
+    public class ModeratorDeleteCommentary : GamesCommentary
+    {
+        [Required]
+        public string ModeratorName { get; set; }
+        [Required]
+        public string Rights_token { get; set; }
+    }
+}
