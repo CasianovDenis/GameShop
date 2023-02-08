d1a48c66f86848bb6edea9ee31fb3af54f674559
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-02-08 15:00:02 +0200

    Added line of code , who remove data from rights_token when exit admin

--- GameShop/ClientApp/src/components/NavMenu/NavMenu.js
+++ GameShop/ClientApp/src/components/NavMenu/NavMenu.js
@@ -78,6 +78,7 @@ export default function NavMenu(props) {
 
         document.cookie = "status_account=; expires = " + now.toUTCString();
 
+       document.cookie = "rights_token=; expires = " + now.toUTCString();
 
        redirect.go('/');
     }
