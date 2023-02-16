53da233f1e2fc9435116ff92a3aeee14cbfe6810
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-02-16 15:17:48 +0200

    Created entity classes for upload video and image

---
+++ GameShop/Models/Mongo/ModeratorUploadImage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.Models.Mongo
+{
+    public class ModeratorUploadImage
+    {
+        [Required]
+        public IFormFile File { get; set; }
+
+
+        [Required]
+        public string GameName { get; set; }
+
+        [Required]
+        public string ModeratorName { get; set; }
+        [Required]
+        public string Rights_token { get; set; }
+    }
+}
