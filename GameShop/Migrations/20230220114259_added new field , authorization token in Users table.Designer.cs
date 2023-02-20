82bdf9f50c22834d88813f92a0ef4988c7715471
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-02-20 16:09:58 +0200

    Added new field from Users table

--- GameShop/Migrations/20230220114259_added new field , authorization token in Users table.Designer.cs
+++ GameShop/Migrations/20230220114259_added new field , authorization token in Users table.Designer.cs
@@ -2,15 +2,17 @@
 using GameShop.Models.DBConnection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace GameShop.Migrations
 {
     [DbContext(typeof(ConString))]
-    partial class ConStringModelSnapshot : ModelSnapshot
+    [Migration("20230220114259_added new field , authorization token in Users table")]
+    partial class addednewfieldauthorizationtokeninUserstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GameShop/Migrations/20230220114259_added new field , authorization token in Users table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameShop.Migrations
+{
+    public partial class addednewfieldauthorizationtokeninUserstable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Authorization_token",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Authorization_token",
+                table: "Users");
+        }
+    }
+}
