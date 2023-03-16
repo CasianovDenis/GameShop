6f961785c78e777ef7026f34e9b13f570e32f09f
Casianov Denis <81120027+AngelBeny@users.noreply.github.com>
2023-03-16 13:09:01 +0200

    Added new spinner in the public files

---
+++ GameShop/ClientApp/src/components/public_files/LoadingSpinner.js
@@ -0,0 +1,10 @@
+﻿import React, { } from 'react';
+
+import style from './LoadingSpinner.module.css'
+
+export default function LoadingSpinner() {
+
+    return (
+        <div className={style.loader}></div>
+        )
+}
